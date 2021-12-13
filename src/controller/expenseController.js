8ebf00e2382e61ca5a592030a327529e8392cd07
sahil <srmultani018@gmail.com>
2021-12-13 19:16:26 +0530

    --deleted unused file

--- src/controller/expenseController.js
+++
@@ -1,8 +0,0 @@
-const expenseService = require('../services/expenseService');
-
-const createExpenseController = async (req, res) => {
-	let response = await expenseService.createExpenseService(req.body);
-	res.send(response);
-};
-
-module.exports = { createExpenseController };
